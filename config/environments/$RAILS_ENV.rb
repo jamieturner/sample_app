552adab17728381a2851ab93be75adc5bb2020b7
Jamie Turner <jamieturner84@hotmail.com>
2013-12-01 12:43:21 +0000

    error

--- config/environments/$RAILS_ENV.rb
+++ config/environments/$RAILS_ENV.rb
@@ -2,7 +2,7 @@ config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'http://mysterious-chamber-5155.herokuapp.com',
+  domain:               'herokuapp.com',
   user_name:            'jrturner992@gmail.com',
   password:             '425jelly',
   authentication:       'plain',
