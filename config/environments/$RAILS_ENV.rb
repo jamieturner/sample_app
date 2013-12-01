2e1345ca83828c8d428dae7a0afec780de97ddda
Jamie Turner <jamieturner84@hotmail.com>
2013-12-01 12:30:00 +0000

    mail config

--- config/environments/$RAILS_ENV.rb
+++ config/environments/$RAILS_ENV.rb
@@ -2,8 +2,8 @@ config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'example.com',
-  user_name:            'jamieturner84@hotmail.com',
+  domain:               'http://mysterious-chamber-5155.herokuapp.com',
+  user_name:            'jjrturner992@gmail.com',
   password:             '425jelly',
   authentication:       'plain',
   enable_starttls_auto: true  }
