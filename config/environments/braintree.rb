d506c30e0675d2efdf2de938de968def73563489
Jamie Turner <jamieturner84@hotmail.com>
2014-01-04 13:59:59 +0000

    Removed more braintree files

--- config/environments/braintree.rb
+++
@@ -1,8 +0,0 @@
-Braintree::Configuration.environment = :sandbox
-Braintree::Configuration.merchant_id = "zm39btd8v5mm62ws"
-Braintree::Configuration.public_key = "pdm38t7nnycrmrpx"
-Braintree::Configuration.private_key = "a074f2341674613fd36e8caeebadc068"
-
-
-PUBLISHABLE_KEY = sk_test_IkzixacJX3zSanfKPSFfetvq
-SECRET_KEY = pk_test_ZDGtg430OrlEmaHnhJFEfCE9
