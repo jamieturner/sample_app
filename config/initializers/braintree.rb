9e5e586820df0ebdee25888e580f195dd1763114
Jamie Turner <jamieturner84@hotmail.com>
2014-01-04 13:52:37 +0000

    removed Braintree initializer

--- config/initializers/braintree.rb
+++
@@ -1,4 +0,0 @@
-Braintree::Configuration.environment = :sandbox
-Braintree::Configuration.merchant_id = "zm39btd8v5mm62ws"
-Braintree::Configuration.public_key = "pdm38t7nnycrmrpx"
-Braintree::Configuration.private_key = "a074f2341674613fd36e8caeebadc068"
